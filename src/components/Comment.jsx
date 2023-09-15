907215dd9f3f120bb4a8337d4f932f8f4591213d
Renan Vieira <reenaanfc@gmail.com>
2023-09-14 23:45:15 -0300

    Estrutura e estilização finalizada

---
+++ src/components/Comment.jsx
@@ -0,0 +1,32 @@
+import { ThumbsUp, Trash } from 'phosphor-react';
+import styles from './Comment.module.css';
+import { Avatar } from './Avatar';
+
+export function Comment(){
+  return(
+    <div className={ styles.comment }>
+      <Avatar hasBorder={ false } src="https://github.com/renanvieira-dev.png" alt="" />
+      <div className={ styles.commentBox }>
+        <div className={ styles.commentContent }>
+          <header>
+            <div className={ styles.authorAndTime }>
+              <strong>Renan Vieira</strong>
+              <time title='11 de Maio às 08:13' dateTime='2022-05-11 08:13:30'>Cerca de 1h atrás</time>
+            </div>
+
+            <button title='Deletar comentário'>
+              <Trash size={24} />
+            </button>
+          </header>
+          <p>Muito bom Devon, parabéns!! 👏👏</p>
+        </div>
+        <footer>
+          <button>
+            <ThumbsUp />
+            Aplaudir <span>20</span>
+          </button>
+        </footer>
+      </div>
+    </div>
+  )
+}
