42c726d3adfd4339ec1e96ff735f65c5ce492bfb
Renan Vieira <reenaanfc@gmail.com>
2023-09-16 20:17:36 -0300

    sistema de comentários ativos

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,43 @@
+/* eslint-disable react/jsx-key */
 import { Header } from "./components/Header";
 import { Post } from './components/Post';
 import { Sidebar } from "./components/Sidebar";
 import './global.css';
 import styles from './App.module.css';
+
+const posts = [
+  {
+    id: 1,
+    author: {
+      avatarUrl: 'https://github.com/renanvieira-dev.png',
+      name: 'Renan vieira',
+      role: 'Web Developer'
+    },
+
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉<a href="#">jane.design/doctorcare</a>' },
+    ],
+    publishedAt: new Date('2022-05-03 20:00:00')
+  },
+
+  {
+    id: 2,
+    author: {
+      avatarUrl: 'https://github.com/maykbrito.png',
+      name: 'Mayk Brito',
+      role: 'Educator @Rocketseat'
+    },
+
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋', },
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀' },
+      { type: 'link', content: '👉<a href="#">jane.design/doctorcare</a>' },
+    ],
+    publishedAt: new Date('2022-05-10 20:00:00')
+  },
+];
 
 export function App() {
   return (
@@ -11,8 +46,16 @@ export function App() {
       <div className={ styles.wrapper }>
         <Sidebar />
         <main>
-          <Post />
-          <Post />
+          {posts.map(post => {
+            return (
+              <Post 
+                key={post.id}
+                author={post.author}
+                content={post.content}
+                publishedAt={post.publishedAt}
+              />
+            )
+          })}
         </main>
       </div>
     </div>
